782f88bbe6f3674e268df8de39a5023f529683fd
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-01-21 20:51:16 -0500

    Correción del BASE_API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_API = "http://localhost:8000/api";
-export const TOKEN = process.env.TOKEN_NAME || "access";
-export const REFRESH = process.env.REFRESH_NAME || "refresh";
-export const USER_ID = process.env.USER_ID_NAME || "id";
+export const BASE_API = process.env.BACKEND_URL;
+export const TOKEN = process.env.TOKEN_NAME;
+export const REFRESH = process.env.REFRESH_NAME;
+export const USER_ID = process.env.USER_ID_NAME;
