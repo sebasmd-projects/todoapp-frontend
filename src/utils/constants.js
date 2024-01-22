b7aa8d7b41883043f386ad271608dac8b1800ee4
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-01-21 21:07:14 -0500

    process.env.BACKEND_URL => https://backend.sebasmoralesd.com/api
    - esta de noche, tengo sueño, y no reconoce el .env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_API = process.env.BACKEND_URL;
+export const BASE_API = "https://backend.sebasmoralesd.com/api";
 export const TOKEN = process.env.TOKEN_NAME;
 export const REFRESH = process.env.REFRESH_NAME;
 export const USER_ID = process.env.USER_ID_NAME;
